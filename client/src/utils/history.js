968ff1ae4a00e092512c834d523da2fd5f26680a
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2022-12-19 02:24:06 +0300

    Create history.js

---
+++ client/src/utils/history.js
@@ -0,0 +1,7 @@
+// exports a new instance of the createBrowserHistory function from the history library, which creates a new history object that can be used to manage the browser's history. This object can be imported and used in other components to navigate the app.
+
+
+
+import { createBrowserHistory } from 'history';
+
+export default createBrowserHistory();
