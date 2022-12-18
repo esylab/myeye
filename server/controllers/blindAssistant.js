48d51b1017eae4c443bea8068db4914eebe61a10
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2022-12-19 01:57:46 +0300

    Update blindAssistant.js

--- server/controllers/blindAssistant.js
+++ server/controllers/blindAssistant.js
@@ -1,77 +1,93 @@
+// Imports the required dependencies, including express for the router and the blindAssistantController for handling the API routes.
+// Defines the API routes and associates them with the appropriate controller methods.
+// Exports the router.
+// The controller methods (e.g. describeSurroundings, playAudio, etc.) will handle the requests made to these routes and return the appropriate data or perform the necessary actions.
 
 
 
 
-const BlindAssistant = require('../models/blindAssistant');
 const blindAssistantService = require('../services/blindAssistant');
 
-// Get a description of the surroundings using computer vision
-exports.describeSurroundings = async (req, res, next) => {
+exports.describeSurroundings = (req, res, next) => {
   try {
-    const description = await blindAssistantService.describeSurroundings();
-    return res.status(200).json({ description });
-  } catch (error) {
-    return next(error);
+    // Use the service to get an audio description of the surroundings
+    const audioDescription = blindAssistantService.getAudioDescription();
+
+    // Return the audio description to the client
+    res.send(audioDescription);
+  } catch (err) {
+    next(err);
   }
 };
 
-// Play audio
-exports.playAudio = async (req, res, next) => {
+exports.playAudio = (req, res, next) => {
   try {
-    await blindAssistantService.playAudio();
-    return res.sendStatus(200);
-  } catch (error) {
-    return next(error);
+    // Use the service to play the audio
+    blindAssistantService.playAudio();
+
+    // Return a success message to the client
+    res.send({ message: 'Audio playback started' });
+  } catch (err) {
+    next(err);
   }
 };
 
-// Pause audio
-exports.pauseAudio = async (req, res, next) => {
+exports.pauseAudio = (req, res, next) => {
   try {
-    await blindAssistantService.pauseAudio();
-    return res.sendStatus(200);
-  } catch (error) {
-    return next(error);
+    // Use the service to pause the audio
+    blindAssistantService.pauseAudio();
+
+    // Return a success message to the client
+    res.send({ message: 'Audio playback paused' });
+  } catch (err) {
+    next(err);
   }
 };
 
-// Play the next track
-exports.nextTrack = async (req, res, next) => {
+exports.nextTrack = (req, res, next) => {
   try {
-    await blindAssistantService.nextTrack();
-    return res.sendStatus(200);
-  } catch (error) {
-    return next(error);
+    // Use the service to play the next track
+    blindAssistantService.nextTrack();
+
+    // Return a success message to the client
+    res.send({ message: 'Playing next track' });
+  } catch (err) {
+    next(err);
   }
 };
 
-// Play the previous track
-exports.prevTrack = async (req, res, next) => {
+exports.prevTrack = (req, res, next) => {
   try {
-    await blindAssistantService.prevTrack();
-    return res.sendStatus(200);
-  } catch (error) {
-    return next(error);
+    // Use the service to play the previous track
+    blindAssistantService.prevTrack();
+
+    // Return a success message to the client
+    res.send({ message: 'Playing previous track' });
+  } catch (err) {
+    next(err);
   }
 };
 
-// Increase the volume
-exports.increaseVolume = async (req, res, next) => {
+exports.increaseVolume = (req, res, next) => {
   try {
-    await blindAssistantService.increaseVolume();
-    return res.sendStatus(200);
-  } catch (error) {
-    return next(error);
+    // Use the service to increase the volume
+    blindAssistantService.increaseVolume();
+
+    // Return a success message to the client
+    res.send({ message: 'Volume increased' });
+  } catch (err) {
+    next(err);
   }
 };
 
-// Decrease the volume
-exports.decreaseVolume = async (req, res, next) => {
+exports.decreaseVolume = (req, res, next) => {
   try {
-    await blindAssistantService.decreaseVolume();
-    return res.sendStatus(200);
-  } catch (error) {
-    return next(error);
+    // Use the service to decrease the volume
+    blindAssistantService.decreaseVolume();
+
+    // Return a success message to the client
+    res.send({ message: 'Volume decreased' });
+  } catch (err) {
+    next(err);
   }
 };
-
