271ab94115025d192e7375c74645f01086cfd0cf
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2022-12-19 03:28:58 +0300

    Update VoiceControlsVolume.js

--- client/src/components/VoiceControlsVolume.js
+++ client/src/components/VoiceControlsVolume.js
@@ -1,5 +1,15 @@
 // This code adds a volume and a rate state variable to the component, as well as two input range elements and their corresponding change handlers. The volume and rate state variables are used to store the current volume and rate values, respectively, and the input range elements allow the user to adjust these values by dragging a slider.
 
+
+
+// This code uses the window.speechSynthesis object and the SpeechSynthesisUtterance constructor to synthesize the audio, and sets the volume and rate properties of the utterance object to the values stored in the volume and rate state variables, respectively. The speak function is then called when the user clicks the Send button to synthesize and play the audio.
+
+// You can then use the speak function in other parts of the component as needed to synthesize and play audio for the user. For example, you could use it to provide feedback to the user when the command is received or when an error occurs
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -40,35 +50,6 @@ const VoiceCommands = () => {
     }
   };
 
-  const handleSend = () => {
-    axios.post('/api/command', { command });
-  };
-
-  const handleVolumeChange = (e) => {
-    setVolume(e.target.value);
-  };
-
-  const handleRateChange = (e) => {
-    setRate(e.target.value);
-  };
-
-  return (
-    <div>
-      <textarea value={command} onChange={(e) => setCommand(e.target.value)} />
-      <div>
-        <label>Volume:</label>
-        <input type="range" min="0" max="1" step="0.1" value={volume} onChange={handleVolumeChange} />
-      </div>
-      <div>
-        <label>Rate:</label>
-        <input type="range" min="0.5" max="2" step="0.1" value={rate} onChange={handleRateChange} />
-      </div>
-      <button onClick={handleListen}>
-        {isListening ? 'Stop Listening' : 'Start Listening'}
-      </button>
-      <button onClick={handleSend}>Send</button>
-    </div>
-  );
-};
-
-export default VoiceCommands;
+  const speak = (text) => {
+    const synth = window.speechSynthesis;
+    const utterance = new Speech
